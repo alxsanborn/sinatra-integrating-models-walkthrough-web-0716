30481dd014937edd2b3eceff3f4438aeaefd6b2f
jasmin47 <alxsanborn@gmail.com>
2016-08-11 17:57:56 -0400

    done

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ get '/' do
 end
 
 post '/' do
-  text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
+  @word_count = @analyzed_text.count_of_words.to_s
+  @vowel_count = @analyzed_text.count_of_vowels.to_s
+  @consonant_count = @analyzed_text.count_of_consonants.to_s
+  @most_common_ltr = @analyzed_text.most_used_letter[0].to_s
+  @common_ltr_num = @analyzed_test.most_used_letter[1].to_s
   erb :results
 end
 
